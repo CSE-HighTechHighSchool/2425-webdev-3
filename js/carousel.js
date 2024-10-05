30c80afb388420dbe290d66d555f539751c7a730
NguyenRich <richardnguyenryx@gmail.com>
2024-10-05 10:46:29 -0400

    Adjustments to Carousel portion
    Added arrows to the problems section, edited titles and class names

--- js/carousel.js
+++ js/carousel.js
@@ -1,11 +1,14 @@
 $(".custom-carousel").owlCarousel({
-    autoWidth: true,
-    loop: true
+  autoWidth: true,
+  loop: false,
+  nav: true,
+  dots: false,
+  rewind: true
+});
+
+$(document).ready(function () {
+  $(".custom-carousel .item").click(function () {
+    $(".custom-carousel .item").not($(this)).removeClass("active");
+    $(this).toggleClass("active");
   });
-  $(document).ready(function () {
-    $(".custom-carousel .item").click(function () {
-      $(".custom-carousel .item").not($(this)).removeClass("active");
-      $(this).toggleClass("active");
-    });
-  });
-  
+});
