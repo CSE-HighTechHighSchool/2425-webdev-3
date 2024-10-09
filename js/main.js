4369cc2f47dc51ffd1219a663652dae3e8adc782
NguyenRich <richardnguyenryx@gmail.com>
2024-10-09 00:41:51 -0400

    Adjustments to Javascript for correction
    Make the active class apply to only an element with the class "clickable"

--- js/main.js
+++ js/main.js
@@ -1,7 +1,29 @@
 function makeActive(event) {
-  var previous = document.getElementsByClassName("active");
-  if (previous.length > 0) {
-     previous[0].className = previous[0].className.replace(" active", "");
+  var previous = document.getElementsByClassName(" active");
+
+  var clickable = event.target;
+
+  if (!clickable.className.includes('clickable')) {
+    var parent = event.target.parentNode;
+    while (!parent.className.includes('clickable')) {
+      parent = parent.parentNode;
+    }
+    clickable = parent;
   }
+
+  if (clickable.className.includes(" active")) {
+      clickable.className = clickable.className.replace(" active", "");
+  } else {
+    clickable.className = clickable.className += " active";
+  }
+  /*
+if (event.target.className.includes('clickable')) {
   event.target.className += " active";
+} else if (event.target.parentElement.className.includes('clickable')) {
+  event.target.parentElement.className += " active";
+} else if (event.target.parentElement.parentElement.className.includes('clickable')) {
+  event.target.parentElement.parentElement.className += " active";
 }
+  */
+}
+
