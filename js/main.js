d87713f03ce5a8b29f2e701051c651e9de922b0f
NguyenRich <richardnguyenryx@gmail.com>
2024-10-28 00:44:40 -0400

    CSS Organization

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 function makeActive(event) {
   var clickable = event.target;
 
+  // Search for the parent element that has the class "clickable"
   if (!clickable.className.includes('clickable')) {
     var parent = event.target.parentNode;
     while (!parent.className.includes('clickable')) {
@@ -10,8 +11,10 @@ function makeActive(event) {
     clickable = parent;
   }
 
+
+  // Add/Remove the active class to the element with class "clickable"
   if (clickable.className.includes(" active")) {
-      clickable.className = clickable.className.replace(" active", "");
+    clickable.className = clickable.className.replace(" active", "");
   } else {
     clickable.className = clickable.className += " active";
   }
