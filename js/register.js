df6b3d0f22650ac2a82acf1a46ffb30e8544c5a0
NguyenRich <richardnguyenryx@gmail.com>
2025-01-14 11:52:32 -0500

    kjhkj

--- js/register.js
+++ js/register.js
@@ -84,7 +84,7 @@ function isEmptyorSpaces(str) {
 function validation(firstName, lastName, email, password) {
   let fNameRegex = /^[a-zA-Z]+$/;
   let lNameRegex = /^[a-zA-Z]+$/;
-  let emailRegex = /^[a-zA-Z]+@[a-zA-Z]+\.(com|org)$/;
+  let emailRegex = /^[a-zA-Z0-9]+@[a-zA-Z]+\.(com|org)$/;
 
   if (isEmptyorSpaces(firstName) || isEmptyorSpaces(lastName) ||
     isEmptyorSpaces(email) || isEmptyorSpaces(password)) {
